d1b6b4c5d9e85cd20b65092990a8d601fbb2149e
takaya <takareo1223@gmail.com>
2015-01-11 14:32:21 +0000

    Finish layout and routes

---
+++ example_user.rb
@@ -0,0 +1,5 @@
+person1={first:"yasu",last:"tamaki"}
+person2={first:"rie",last:"tamaki"}
+person3={first:"takaya",last:"tamaki"}
+params={father:person1,mother:person2,child:person3}
+puts params[:father][:first] ,params[:mother][:first] ,params[:child][:first]
